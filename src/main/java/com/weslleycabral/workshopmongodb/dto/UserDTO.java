4df3df0c63277656d846b50b6246f99609bbb402
Weslley Cabral <weslley.deve@gmail.com>
2024-01-19 13:29:43 -0300

    Getting a user by id

---
+++ src/main/java/com/weslleycabral/workshopmongodb/dto/UserDTO.java
@@ -0,0 +1,60 @@
+package com.weslleycabral.workshopmongodb.dto;
+
+import com.weslleycabral.workshopmongodb.entities.User;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class UserDTO implements Serializable {
+
+    private static final Long serialVersionUID = 1L;
+
+    private String id;
+    private String name;
+    private String email;
+
+    public UserDTO() {}
+
+    public UserDTO(User user) {
+        this.id = user.getId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDTO userDTO = (UserDTO) o;
+        return Objects.equals(id, userDTO.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
